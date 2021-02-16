74cee673ebc6c680b049b677130b8bd1dd1fbc8d
fastxiaohu <792773585@qq.com>
2021-02-16 11:22:42 +0800

    归并排序

---
+++ src/main/java/com/xiaohuxiong/sort/MergerSort.java
@@ -0,0 +1,69 @@
+package com.xiaohuxiong.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 归并排序
+ */
+public class MergerSort {
+
+
+     //递归归排序
+    //通过递归将arr变成2边是有序的 [l,r]上是有序的
+    public void sortByRecursion(List<Integer> list,int l,int r){
+        if (r == l){
+            return;
+        }
+        int mid = l + ((r-l) >> 1) ;
+        sortByRecursion(list,l,mid);
+        sortByRecursion(list,mid+1,r);
+    }
+
+    //非递归排序
+    public void sortByNormal(List<Integer> list){
+        int size = list.size();
+        if ((size==0)||(size==1)){
+            return;
+        }
+        int mergerSize = 1;
+        while (mergerSize < size){
+            int l  = 0;
+           while (l < size){
+               int m = l+mergerSize -1;
+               if (m >= size){
+                   break;
+               }
+               int r = Math.min(m + mergerSize, size - 1);
+               merger(list,l,m,r);
+               l = r+1;
+           }
+           if (mergerSize > size/2){
+               break;
+           }
+            mergerSize <<= 1;
+        }
+
+    }
+
+    public static void merger(List<Integer> list,int l, int mid, int r){
+        List<Integer> occup = new ArrayList<>();
+        int i = 0;
+        int p1 = l;
+        int p2 = mid +1;
+
+        while (p1 <= mid && p2 <= r ){
+            occup.add(i++,list.get(p1) >= list.get(p2)?list.get(p2++):list.get(p1++));
+        }
+
+        while (p1 <= mid){
+            occup.add(i++,list.get(p1++));
+        }
+
+        while (p2 <= r){
+            occup.add(i++,list.get(p2++));
+        }
+    }
+
+
+}
---
+++ src/main/java/com/xiaohuxiong/sort/MergerSortByOnline.java
@@ -0,0 +1,61 @@
+package com.xiaohuxiong.sort;
+
+public class MergerSortByOnline {
+
+    public static void mergeSort(int[] arr){
+
+        if(arr == null || arr.length < 2)
+            return;
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(int[] arr, int l, int r) {
+        //终止条件
+        if(l == r)
+            return;
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);//拆分左半部分
+        mergeSort(arr, mid + 1, r);//拆分右半部分
+        merge(arr, l, mid, r);//每次拆分完之后就合并，合并的都是有序的数组，所以比较块
+    }
+
+    //合并的方法有点类似于连个有序链表的合并过程
+    private static void merge(int[] arr, int l, int mid, int r) {
+
+        int[] help = new int[r - l + 1];//创建一个辅助数组
+        int i = 0;
+        int p1 = l;//做部分指针
+        int p2 = mid +1;//右部分指针
+        while(p1 <=mid && p2 <= r)
+        {
+            help[i++] = arr[p1] < arr[p2]? arr[p1++] : arr[p2++];
+        }
+        //两个while循环用来处理剩余部分，必定只执行一个
+        while(p1 <= mid)
+            help[i++] = arr[p1++];
+        while(p2 <= r)
+            help[i++] = arr[p2++];
+        for(int j = 0; j < help.length; j++)//合并成功的部分整理到原数组
+            arr[l + j] = help[j];
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue){
+
+        int[] arr = new int[(int)((maxSize + 1) * Math.random())];//Math.random()产生的是一个[0,1)随机数
+        for(int i = 0; i < arr.length; i++)
+            arr[i] = (int)(((maxValue + 1) * Math.random()) - ((maxValue + 1) * Math.random()));
+        return arr;
+    }
+    public static void main(String[] args) {
+	/*	int[] arr = {1,6,3,2,8,0,9};
+		mergeSort(arr);
+		for(int i = 0; i < arr.length; i++)
+			System.out.println(arr[i]);*/
+        int[] arr = generateRandomArray(20, 16);
+        mergeSort(arr);
+        for(int i = 0; i < arr.length; i++)
+            System.out.println(arr[i]);
+
+    }
+
+}
